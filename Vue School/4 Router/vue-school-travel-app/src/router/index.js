d3a4bd2de5458accd228b47905d6a04c5fbbceb0
CristobalHdz <cristobalhdzch@gmail.com>
2021-09-30 13:59:01 -0600

    Finish router 4 ch 3

--- Vue School/4 Router/vue-school-travel-app/src/router/index.js
+++ Vue School/4 Router/vue-school-travel-app/src/router/index.js
@@ -1,44 +1,50 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/Home.vue";
+import sourceData from "@/data.json";
 
 const routes = [
+  { path: "/", name: "Home", component: Home, },
   {
-    path: "/", name: "Home", component: Home,
+    path: '/destination/:id/:slug',
+    name: 'destination.show',
+    component: () => import('@/views/DestinationShow.vue'),
+    props: route => ({ ...route.params, id: parseInt(route.params.id) }),
+    beforeEnter(to) {
+      const exists = sourceData.destinations.find(
+        destination => destination.id === parseInt(to.params.id)
+      )
+      if (!exists) return {
+        name: 'NotFound',
+        // allows keeping the URL while rendering a different page
+        params: { pathMatch: to.path.split('/').slice(1) },
+        query: to.query,
+        hash: to.hash,
+      }
+    },
+    children: [
+      {
+        path: ':experienceSlug',
+        name: 'experience.show',
+        component: () => import('@/views/ExperienceShow.vue'),
+        props: route => ({ ...route.params, id: parseInt(route.params.id) })
+      }
+    ]
   },
   {
-    path: "/brazil",
-    name: "Brazil",
-    component: () =>
-      import(/*webpackChunkName: "brazil" */ "../views/Brazil.vue"),
-  },
-  {
-    path: "/hawaii",
-    name: "Hawaii",
-    component: () =>
-      import(/*webpackChunkName: "hawaii" */ "../views/Hawaii.vue"),
-  },
-  {
-    path: "/jamaica",
-    name: "Jamaica",
-    component: () =>
-      import(/*webpackChunkName: "jamaica" */ "../views/Jamaica.vue"),
-  },
-  {
-    path: "/panama",
-    name: "Panama",
-    component: () =>
-      import(/*webpackChunkName: "panama" */ "../views/Panama.vue"),
-
-  },
-  {
-    path: '/destination/:id',
-    component: () => import('@/views/DestinationShow.vue')
-  },
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: () => import('@/views/NotFound.vue')
+  }
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
+  scrollBehavior(savedPosition) {
+    return savedPosition || new Promise((resolve) => {
+      setTimeout(() => resolve({ top: 0, behavior: "smooth" }), 300)
+    })
+  }
 });
 
 export default router;
--- Vue School/4 Router/vue-school-travel-app/src/views/Panama.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="panama">
-    <h1>Pamana</h1>
-  </div>
-</template>
